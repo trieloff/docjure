3a2d9e4718a782879baf48e372842f2b23dc81d2
Lars Trieloff <lars@trieloff.net>
2016-03-15 20:04:14 +0100

    Added test2junit dependency

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [org.apache.poi/poi "3.13"]
                  [org.apache.poi/poi-ooxml "3.13"]]
   :plugins [[lein-difftest "2.0.0"]
+            [test2junit "1.1.2"]
             [lein-maven-s3-wagon "0.2.4"]]
   :deploy-repositories {"private" {:url "s3://leinrepo/releases/"
                                    :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
@@ -19,6 +20,7 @@
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :test {:global-vars  {*warn-on-reflection* false}
                     :dependencies [[com.cemerick/pomegranate "0.3.0"]]}}
+  :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
   :aliases {"all" ["with-profile" "1.3:1.4:1.5:1.6:1.7:1.8"]}
   :global-vars {*warn-on-reflection* true})
 
